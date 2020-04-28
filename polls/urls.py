af585290c0e407b367b665ddc248584a683fc720
Zamanax <zamax27@gmail.com>
2020-04-28 16:04:15 +0200

    Updated the paths and the css file for mockup 5
    Co-authored-by: Max <maximecheniour@gmail.com>
    Co-authored-by: whitecanarystan <lauryduty@gmail.com>
    Co-authored-by: Octave <thepurpl3shad0w@gmail.com>
    Co-authored-by: yash-13 <yash.ori498@gmail.com>

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.mo1, name='index'),
+    path("mo1", views.mo1, name="mo1"),
+    path("mo2", views.mo2, name="mo2"),
+    path("mo3", views.mo3, name="mo3"),
+    path("mo4", views.mo4, name="mo4"),
     path("mo5", views.mo5, name="mo5"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,24 +3,28 @@ import requests
 
 from . import forms
 
+def mo1(request):
+    return render(request, "polls/mo1.html")
 
-def index(request):
-    return render(request, "polls/index.html")
+def mo2(request):
+    pass
 
+def mo3(request):
+    pass
+
+def mo4(request):
+    pass
 
 def mo5(request):
     if request.method == "POST":
         form = forms.mo5Form(request.POST)
         if form.is_valid():
-            print(form.cleaned_data["sentence"])
-
             value = form.cleaned_data["sentence"]
             headers = {'Agent-Token': 'aeb7fece4a1f3cb0c063c710a931e832'}
             params = {"sentence": value}
             url = 'https://www.viky.ai/api/v1/agents/babybel/pronouns/interpret.json'
 
             response = requests.get(url, headers=headers, params=params).json()
-            print(response)
 
             if len(response["interpretations"]) != 0:
                 return render(request, "polls/mo5.html", {"value": value, "response": response, "interpretations": True })
