9a75ae77439be88e2a18b31bde40caefb51e4483
Zamanax <zamax27@gmail.com>
2020-04-29 18:47:18 +0200

    mo4 basically done for now
    Co-authored-by: whitecanarystan <lauryduty@gmail.com>
    Co-authored-by: Max <maximecheniour@gmail.com>
    Co-authored-by: yash-13 <yash.ori498@gmail.com>

--- polls/forms.py
+++ polls/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
+class mo4Form(forms.Form):
+    word = forms.CharField(label='word', max_length=100)
+
 class mo5Form(forms.Form):
     sentence = forms.CharField(label='sentence', max_length=100)
--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 import requests
 
 from . import forms
 
+
 def mo1(request):
     return render(request, "polls/mo1.html")
+
 
 def mo2(request):
     pass
 
+
 def mo3(request):
     pass
 
+
 def mo4(request):
-    pass
+    if request.method == "POST":
+        form = forms.mo4Form(request.POST)
+        if form.is_valid():
+            value = form.cleaned_data["word"]
+            headers = {'Agent-Token': 'aeb7fece4a1f3cb0c063c710a931e832'}
+            params = {"sentence": value, "verbose": "true"}
+            url = 'https://www.viky.ai/api/v1/agents/babybel/pronouns/interpret.json'
+
+            response = requests.get(url, headers=headers, params=params).json()
+            return HttpResponse("It is a pronoun" if len(response["interpretations"]) else "It is not")
+    return render(request, "polls/mo4.html")
+
 
 def mo5(request):
     if request.method == "POST":
@@ -21,18 +36,32 @@ def mo5(request):
         if form.is_valid():
             value = form.cleaned_data["sentence"]
             headers = {'Agent-Token': 'aeb7fece4a1f3cb0c063c710a931e832'}
-            params = {"sentence": value}
+            params = {"sentence": value, "verbose": "true"}
             url = 'https://www.viky.ai/api/v1/agents/babybel/pronouns/interpret.json'
 
             response = requests.get(url, headers=headers, params=params).json()
 
             if len(response["interpretations"]) != 0:
-                return render(request, "polls/mo5.html", {"value": value, "response": response, "interpretations": True })
+                match = []
+                listOfWords = []
+                for interpretation in response["interpretations"]:
+                    word = list(filter(
+                        lambda word: word["match"] != None, interpretation["explanation"]["highlight"]["words"]))[0]
+                    match = [word["match"]] + match
+                    listOfWords = [word["word"]] + listOfWords
+                wordCounter = 0
+                words = []
+                for i in range(len(value.split(" "))):
+                    if wordCounter < len(listOfWords) and value.split(" ")[i] == listOfWords[wordCounter]:
+                        words.append(
+                            {"word": listOfWords[wordCounter], "match": match[wordCounter]})
+                        wordCounter += 1
+                    else:
+                        words.append(
+                            {"word": value.split(" ")[i], "match": None})
             else:
-                return render(request, "polls/mo5.html", {"value": value, "response": response, "interpretations": False})
+                words = map(lambda word: {"word": word,
+                                          "match": None}, value.split(" "))
 
-        else:
-            print("There was an invalid form")
-            return render(request, "polls/mo5.html")
-    else:
-        return render(request, "polls/mo5.html")
+            return render(request, "polls/mo5.html", {"words": words, "interpretations": len(response["interpretations"]) != 0})
+    return render(request, "polls/mo5.html")
